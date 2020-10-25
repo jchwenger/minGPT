491cd8c9e0b0c23de6c25ccc3d7eeab5ca9d209a
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-10-25 17:19:05 +0000

    sample | rough draft working for both bytes & chars

--- sample.py
+++ sample.py
@@ -1,9 +1,20 @@
+
 import os
 import json
 import torch
+import logging
 import argparse
+
 from mingpt.utils import sample
+from mingpt.utils import load_json
+from mingpt.utils import check_name
+from mingpt.utils import print_state_dict
+
 from mingpt.model import GPT, GPTConfig
+
+from char_dataset import BytesDataset
+
+logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser(
     description="",
@@ -26,24 +37,13 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-assert os.path.isfile(args.model), f"{args.model} not found"
-mod_config = os.path.splitext(args.model)[0] + ".json"
-assert os.path.isfile(mod_config), f"{mod_config} not found"
-vocab_config = os.path.splitext(args.model)[0] + ".vocab.json"
-assert os.path.isfile(mod_config), f"{vocab_config} not found"
+args.model, model_name = check_name(args.model)
 
-with open(mod_config) as i:
-    data = json.load(i)
+data = load_json(model_name + ".json")
 
-mconf = GPTConfig(
-    data["vocab_size"],
-    data["block_size"],
-    n_layer=data["n_layer"],
-    n_head=data["n_head"],
-    n_embd=data["n_embd"],
-)
-model = GPT(mconf)
-model.load_state_dict(torch.load(args.model))
+model = GPT(GPTConfig(**data))
+
+model.load_state_dict(torch.load(args.model)["model_state_dict"])
 
 device = "cpu"
 if torch.cuda.is_available():
@@ -51,26 +51,27 @@ if torch.cuda.is_available():
 
 model.to(device)
 
-with open(vocab_config) as i:
-    stoi = json.load(i)
+stoi = load_json(model_name + ".vocab.json")
+bytes_level = True if len(stoi) == 1 and "bytes" in stoi else False
+if not bytes_level:
     itos = {int(v): k for k, v in stoi.items()}
 
-# Print model's state_dict
-print("-" * 40)
-msg = "Model's state_dict:"
-print(msg)
-print("-" * len(msg))
-for param_tensor in model.state_dict():
-    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
-print("-" * 40)
+print_state_dict(model)
 
-print("inferring on device:", device)
+logger.info("inferring on device:", device)
 
-context = "O God, O God!" if args.context is not None else args.context
+context = "o god, o god!" if args.context is None else args.context
 
-x = torch.tensor([stoi[s] for s in context], dtype=torch.long)[
-    None, ...
-].to(device)
+if bytes_level:
+    context = BytesDataset.encode(context)
+else:
+    context = [stoi[s] for s in context]
+x = torch.tensor(context, dtype=torch.long)[None, ...].to(device)
 y = sample(model, x, 2000, temperature=1.0, sample=True, top_k=10)[0]
-completion = "".join([itos[int(i)] for i in y])
+
+if bytes_level:
+    completion = BytesDataset.decode(y)
+else:
+    completion = "".join([itos[int(i)] for i in y])
+
 print(completion)
