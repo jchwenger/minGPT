13f6b35457a540b8c0d93eba15bdf1d599015380
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-10-25 17:06:55 +0000

    trainer | saving fn expanded, keeping plain & distributed model versions in class, ability to load saved optimizer, printing sample during training samples during progress

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -50,53 +50,98 @@ class TrainerConfig:
 
 
 class Trainer:
-    def __init__(self, model, train_dataset, test_dataset, config):
+    def __init__(self, model, train_dataset, test_dataset, config, opt=None):
         self.model = model
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
         self.config = config
 
         # take over whatever gpus are on the system
-        self.device = 'cpu'
+        self.device = "cpu"
         if torch.cuda.is_available():
             self.device = torch.cuda.current_device()
-            self.urmodel = self.model
             self.model = torch.nn.DataParallel(self.model).to(self.device)
+        self.urmodel = (
+            self.model.module if hasattr(self.model, "module") else self.model
+        )
+        self.optimizer = self.urmodel.configure_optimizers(config)
+        if opt is not None:
+            logger.info("loading optimizer dict")
+            # [logger.info(f"{k}: {v}") for k, v in opt.items()]
+            self.optimizer.load_state_dict(opt)
 
     def save_checkpoint(self):
         # DataParallel wrappers keep raw model object in .module attribute
-        raw_model = self.model.module if hasattr(self.model, "module") else self.model
         logger.info("saving %s", self.config.ckpt_path)
-        torch.save(raw_model.state_dict(), self.config.ckpt_path)
+        self.urmodel.save(self.config.ckpt_path)
+        save_json(self.train_dataset.stoi, self.config.ckpt_path + ".vocab.json")
+        save_json(self.config.as_dict(), self.config.ckpt_path + ".train.json")
+        torch.save(
+            {
+                "model_state_dict": self.urmodel.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict(),
+            },
+            self.config.ckpt_path + ".pt",
+        )
 
-    def train(self):
+    def train(self, sample_every=0):
         model, config = self.model, self.config
-        raw_model = model.module if hasattr(self.model, "module") else model
-        optimizer = raw_model.configure_optimizers(config)
 
         def run_epoch(split):
-            is_train = split == 'train'
+            is_train = split == "train"
             model.train(is_train)
             data = self.train_dataset if is_train else self.test_dataset
-            loader = DataLoader(data, shuffle=True, pin_memory=True,
-                                batch_size=config.batch_size,
-                                num_workers=config.num_workers)
+            loader = DataLoader(
+                data,
+                shuffle=True,
+                pin_memory=True,
+                batch_size=config.batch_size,
+                num_workers=config.num_workers,
+            )
 
             losses = []
-            pbar = tqdm(enumerate(loader), total=len(loader)) if is_train else enumerate(loader)
+            pbar = (
+                tqdm(enumerate(loader), total=len(loader))
+                if is_train
+                else enumerate(loader)
+            )
             for it, (x, y) in pbar:
 
-                if it != 0 and it % 100 == 0:
+                # https://discuss.pytorch.org/t/current-step-from-optimizer/19370/3
+                if (
+                    "step"
+                    in self.optimizer.state[
+                        self.optimizer.param_groups[0]["params"][-1]
+                    ]
+                ):
+                    step = self.optimizer.state[
+                        self.optimizer.param_groups[0]["params"][-1]
+                    ]["step"]
+                else:
+                    step = 0
+
+                if sample_every > 0 and step != 0 and step % sample_every == 0:
                     context = "\n"
+
                     inp = torch.tensor(
-                        [self.train_dataset.stoi[s] for s in context],
-                        dtype=torch.long
-                    )[None,...].to(self.device)
-                    outp = sample(self.urmodel, inp, 200, temperature=1.0, sample=True, top_k=10)[0]
-                    completion = ''.join([self.train_dataset.itos[int(i)] for i in outp])
-                    print("-"*40)
+                        self.train_dataset.encode(context), dtype=torch.long
+                    )[None, ...].to(self.device)
+                    # inp = torch.tensor(
+                    #     [self.train_dataset.stoi[s] for s in context], dtype=torch.long
+                    # )[None, ...].to(self.device)
+
+                    outp = sample(
+                        self.urmodel, inp, 200, temperature=1.0, sample=True, top_k=10
+                    )[0]
+
+                    completion = self.train_dataset.decode(outp, errors="replace")
+                    # completion = "".join(
+                    #     [self.train_dataset.itos[int(i)] for i in outp]
+                    # )
+
+                    print("-" * 40)
                     print(completion)
-                    print("-"*40)
+                    print("-" * 40)
 
                 # place data on the correct device
                 x = x.to(self.device)
@@ -105,7 +150,8 @@ class Trainer:
                 # forward the model
                 with torch.set_grad_enabled(is_train):
                     logits, loss = model(x, y)
-                    loss = loss.mean() # collapse all losses if they are scattered on multiple gpus
+                    # collapse all losses if they are scattered on multiple gpus
+                    loss = loss.mean()
                     losses.append(loss.item())
 
                 if is_train:
@@ -113,40 +159,54 @@ class Trainer:
                     # backprop and update the parameters
                     model.zero_grad()
                     loss.backward()
-                    torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
-                    optimizer.step()
+                    torch.nn.utils.clip_grad_norm_(
+                        model.parameters(), config.grad_norm_clip
+                    )
+                    self.optimizer.step()
 
                     # decay the learning rate based on our progress
                     if config.lr_decay:
-                        self.tokens += (y >= 0).sum() # number of tokens processed this step (i.e. label is not -100)
+                        self.tokens += (
+                            y >= 0
+                        ).sum()  # number of tokens processed this step (i.e. label is not -100)
                         if self.tokens < config.warmup_tokens:
                             # linear warmup
-                            lr_mult = float(self.tokens) / float(max(1, config.warmup_tokens))
+                            lr_mult = float(self.tokens) / float(
+                                max(1, config.warmup_tokens)
+                            )
                         else:
                             # cosine learning rate decay
-                            progress = float(self.tokens - config.warmup_tokens) / float(max(1, config.final_tokens - config.warmup_tokens))
-                            lr_mult = max(0.1, 0.5 * (1.0 + math.cos(math.pi * progress)))
+                            progress = float(
+                                self.tokens - config.warmup_tokens
+                            ) / float(
+                                max(1, config.final_tokens - config.warmup_tokens)
+                            )
+                            lr_mult = max(
+                                0.1, 0.5 * (1.0 + math.cos(math.pi * progress))
+                            )
                         lr = config.learning_rate * lr_mult
-                        for param_group in optimizer.param_groups:
-                            param_group['lr'] = lr
+                        for param_group in self.optimizer.param_groups:
+                            param_group["lr"] = lr
                     else:
                         lr = config.learning_rate
 
                     # report progress
-                    pbar.set_description(f"epoch {epoch+1} iter {it}: train loss {loss.item():.5f}. lr {lr:e}")
+                    pbar.set_description(
+                        f"epoch {epoch+1} iter {step}: train loss {loss.item():.5f}. lr {lr:e}"
+                    )
 
             if not is_train:
                 test_loss = float(np.mean(losses))
                 logger.info("test loss: %f", test_loss)
                 return test_loss
 
-        best_loss = float('inf')
-        self.tokens = 0 # counter used for learning rate decay
+        best_loss = float("inf")
+        self.tokens = 0  # counter used for learning rate decay
         for epoch in range(config.max_epochs):
 
-            run_epoch('train')
+            run_epoch("train")
             if self.test_dataset is not None:
-                test_loss = run_epoch('test')
+                test_loss = run_epoch("test")
 
             # supports early stopping based on the test loss, or just save always if no test set is provided
             good_model = self.test_dataset is None or test_loss < best_loss
